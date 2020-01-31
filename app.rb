fae193002dfd567bb606ef6762db332084c859ae
Anthony Tilelli <anthony@tilelli.me>
2020-01-31 14:32:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,9 @@ class App < Sinatra::Base
     "Goodbye, #{params[:name]}."
   end
   
-  get "/multiply/:num1/:num2"
+  get "/multiply/:num1/:num2" do
+    params[:num1].to_i * params[:num2].to_i
+  end
   # Code your final two routes here:
 
 end
